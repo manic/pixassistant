293a29acf4f25f02bc370b4de7444605b4252b73
Manic Chuang <manic@pixnet.tw>
2011-03-29 11:05:42 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pixassistant::Application.config.secret_token = '99c30a00e6b066a707ce61812c84ebe03aa2a532c7dc2de4ebccd5f76b94e15f60f78f7a18e496d4796ccd380e0944d1528eb510272532017bc8bb046acf861c'
