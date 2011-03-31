852b0587cb0a3ca1d379e97f36d93783fcd9d375
Manic Chuang <manic@pixnet.tw>
2011-03-31 16:07:03 +0800

    Add funcs and spec

--- app/models/assistance.rb
+++ app/models/assistance.rb
@@ -18,4 +18,14 @@ class Assistance < ActiveRecord::Base
   belongs_to :assistant, :class_name => "User", :conditions => ['assistant_id != ?', 0]
 
   scope :named, lambda { |name| where(:assistant_name => name.to_s) }
+
+  validates_presence_of :master_id
+  validates_format_of :assistant_name, :with => /^[0-9a-zA-Z]+$/i, :on => :create
+  validates_each :assistant_name do |record, attr, value|
+    begin
+      open("http://emma.pixnet.cc/users/#{value.to_s}")
+    rescue
+      record.errors.add attr, '不是 PIXNET 的使用者'
+    end
+  end
 end
--- spec/models/assistance_spec.rb
+++ spec/models/assistance_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
+def add_user(name)
+  User.create({
+    :login => name.to_s,
+    :email => "#{name}@pixnet.net",
+    :nickname => name.to_s,
+    :identity_url => "https://openid.pixnet.cc/u/#{name}"
+  })
+end
+
 describe Assistance do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @user = add_user('manic')
+  end
+
+  it "fails validation with no assistant_name" do
+    Assistance.new.should have(2).error_on(:assistant_name) # empty and no such user
+  end
+
+  it "fails validation with no master_id" do
+    Assistance.new.should have(1).error_on(:master_id)
+  end
+
+  it "fails validation with valid assistant_name but no such user" do
+    # "www" is not a valid account name in PIXNET so there is no such user.
+    Assistance.new(:assistant_name => "www").should have(1).error_on(:assistant_name)
+  end
+
+  it "passes validation with correct assistant_name" do
+    Assistance.new(:assistant_name => "manic").should have(:no).error_on(:assistant_name)
+  end
+
+  it "passes validation with master_id" do
+    Assistance.new(:master_id => 1).should have(:no).error_on(:master_id)
+  end
+
 end
