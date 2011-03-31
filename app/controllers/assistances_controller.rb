875933bc58ad3285cc4b8935e43f10d234de7e9d
Manic Chuang <manic@pixnet.tw>
2011-03-31 17:22:58 +0800

    assistances CRUD

--- app/controllers/assistances_controller.rb
+++ app/controllers/assistances_controller.rb
@@ -3,7 +3,7 @@ class AssistancesController < ApplicationController
   before_filter :login_required
 
   def index
-    @assistance = Assistance.new
+    @new_assistance = Assistance.new
     @assistances = current_user.assistances
   end
 
@@ -12,6 +12,25 @@ class AssistancesController < ApplicationController
     if @assistance.save
       redirect_to assistances_path
     end
+  end
+
+  def show
+    @new_assistance = Assistance.new
+    @assistances = current_user.assistances
+    @assistance = Assistance.find(params[:id])
+    render :index
+  end
+
+  def update
+    @assistance = Assistance.find(params[:id])
+    @assistance.update_attributes(params[:assistance])
+    redirect_to assistance_path(@assistance)
+  end
+
+  def destroy
+    @assistance = Assistance.find(params[:id])
+    @assistance.destroy
+    redirect_to assistances_path
   end
 
 end
