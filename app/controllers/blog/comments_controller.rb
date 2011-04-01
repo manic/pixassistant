c0b0a2589412d386e8e48a9804d819b6767dadad
Manic Chuang <manic@pixnet.tw>
2011-04-01 14:52:22 +0800

    blog/comments index basic layout

--- app/controllers/blog/comments_controller.rb
+++ app/controllers/blog/comments_controller.rb
@@ -1,6 +1,10 @@
 class Blog::CommentsController < ApplicationController
 
+  before_filter :login_required
+
   def index
+    ret = JSON.parse(current_user.pixnet.client.get('/blog/comments?per_page=10').body)
+    @comments = ret["comments"]
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,39 @@
 module ApplicationHelper
+
+  def comment_author_avatar(comment)
+    if 'pixnet' == comment["author_login_type"]
+      avatar = comment['user']['avatar']
+    elsif 'openid' == comment['author_login_type']
+      avatar = comment['openid_associate']['avatar']
+    else
+      avatar = 'http://s.pimg.tw/default/avatar/user/0/zoomcrop/50x50.jpg'
+    end
+    return image_tag(avatar, :widht => 50, :height => 50, :alt => comment['author'])
+  end
+
+  def comment_author_icon(comment)
+    if 'pixnet' == comment["author_login_type"]
+      icon = "//s.pixfs.net/f.pixnet.net/comment/images/openid-pixnet-icon.gif"
+    elsif 'openid' == comment['author_login_type']
+      icon = "//s.pixfs.net/f.pixnet.net/comment/images/openid-#{comment['openid_associate']['type']}-icon.gif"
+    else
+      icon = ''
+    end
+
+    if icon.empty?
+      return ''
+    else
+      return image_tag(icon, :width => 16, :height => 16)
+    end
+  end
+
+  def comment_author_link(comment)
+    link = ''
+    if 'pixnet' == comment["author_login_type"]
+      link = "#{comment['user']['link']}/blog"
+    else
+      link = "javascript:;"
+    end
+    return link_to(comment["author"], link, :target => "_blank")
+  end
 end
