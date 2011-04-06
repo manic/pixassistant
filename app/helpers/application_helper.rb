3b18f96a7a09a5340cb82d9b4b21280e42e3e1d3
Manic Chuang <manic@pixnet.tw>
2011-04-06 15:12:36 +0800

    use to_s

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@ module ApplicationHelper
 
   def comment_author_avatar(comment)
     if 'pixnet' == comment["author_login_type"]
-      avatar = comment['user']['cavatar']
+      avatar = comment['user']['cavatar'].to_s
     elsif 'openid' == comment['author_login_type']
-      avatar = comment['openid_associate']['avatar']
+      avatar = comment['openid_associate']['avatar'].to_s
     else
       avatar = 'http://s.pimg.tw/default/avatar/user/0/zoomcrop/50x50.jpg'
     end
-    return image_tag(avatar, :widht => 50, :height => 50, :alt => comment['author'])
+    return image_tag(avatar, :width => 50, :height => 50, :alt => comment['author'].to_s)
   end
 
   def comment_author_icon(comment)
